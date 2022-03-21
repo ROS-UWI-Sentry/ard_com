9ff07c6471ff896c45880fa83f98ee17b9d1a8b9
uwi-sentry-agx <uwi@uwi.com>
2022-03-21 17:12:21 -0400

    first version of receive data back from arduino

--- scripts/ard_com_rec.py
+++ scripts/ard_com_rec.py
@@ -14,10 +14,10 @@ from std_msgs.msg import Float32
 from twist_to_motor_rps.msg import Num
 
 # Downsampling
-#global ii
-#ii=0
-#global runn
-#runn=0
+global ii
+ii=0
+global runn #key 0: startup, 1: ready to read, 2: ready to write
+runn=0
                         ###note###
 # ____________________________________________________
 #|ROS node for communicating with the serial port     |
@@ -37,44 +37,58 @@ def callback(data):
     #expecting a string in format s0.xxxx,s0.xxxx
     #where s is the sign (positve + or negative -)
     #and x is the numerical value)
+
+
+#wait for a readcommand
+    y=ser.readline()
+    if runn==1:
+        if len(y)>5:
+            rospy.loginfo("speed: "+y)
+            runn=2 #set we have read something go ahead to write.
     
-    format_left = "{:.4f}".format(data.num[0])
-    format_right = "{:.4f}".format(data.num[1])
+            format_left = "{:.4f}".format(data.num[0])
+            format_right = "{:.4f}".format(data.num[1])
     
-    if format_left == "-0.0000":
-        format_left="+0.0000"
+            if format_left == "-0.0000":
+                format_left="+0.0000"
 
 
-    if data.num[0]==0:
-       format_left="+"+format_left
+            if data.num[0]==0:
+               format_left="+"+format_left
 
-    if data.num[0]>0:
-       format_left="+"+format_left
+            if data.num[0]>0:
+               format_left="+"+format_left
 
 
 
-    if format_right=="-0.0000":
-        format_right="+0.0000"
+            if format_right=="-0.0000":
+                format_right="+0.0000"
 
-    if data.num[1]==0:
-        format_right="+"+format_right
+            if data.num[1]==0:
+                format_right="+"+format_right
     
-    if data.num[1]>0:
-        format_right="+"+format_right
+            if data.num[1]>0:
+                format_right="+"+format_right
 
 
 
-    x = ':1,'+format_left+','+format_right
+            x = ':1,'+format_left+','+format_right
    
-    #rospy.loginfo("serial write data: " + x)
-#    ii=ii+1
-#    if ii>20:
-#        rospy.loginfo("serial write data: " + x)
-#        ser.write(x)
-#        ii=0
+            #rospy.loginfo("serial write data: " + x)
+        if runn==2:
+            #ii=ii+1
+            #if ii>10:
+            rospy.loginfo("serial write data: " + x)
+            ser.write(x)
 
-    rospy.loginfo("serial write data: " + x)
-    ser.write(x)
+            rate = rospy.Rate(3000)
+            rate.sleep()
+            runn=1
+
+            #ii=0
+
+        #rospy.loginfo("serial write data: " + x)
+        #ser.write(x)
        
 
 #this function is for subscribing to messages
@@ -105,16 +119,25 @@ if __name__=='__main__':
         bytesize=serial.EIGHTBITS,
         timeout=0.1
     )
-#    if runn==0:
-#        x = ':1,+0.0000,+0.0000'
-#        ser.write(x)
-#        runn=1
+#on startup brake
+    if runn==0:
+        x = ':1,+0.0000,+0.0000'
+        ser.write(x)
+        runn=1
+
+#wait for a readcommand
+#    y=ser.readline()
+#    if runn==1:
+#        if len(y)>5:
+#            rospy.loginfo("speed: "+y)
+#            runn=2 #set we have read something go ahead to write.
+
+
 
 #To ensure the wheels are not spinning at startup
-    x = ':1,+0.0000,+0.0000'
-    ser.write(x)
-
-  
+    #x = ':1,+0.0000,+0.0000'
+    #ser.write(x)
+    
     #start the subscriber
     try:
         listener()
